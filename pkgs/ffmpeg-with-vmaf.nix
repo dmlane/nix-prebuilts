85de2a583f2987375c9bb3008033038b3e404731
David Lane <git2018@dmlane.net>
2025-09-14 17:47:21 +0100

    Latest

--- pkgs/ffmpeg-with-vmaf.nix
+++ pkgs/ffmpeg-with-vmaf.nix
@@ -1,6 +1,5 @@
 { pkgs }:
 pkgs.ffmpeg-full.overrideAttrs (old: {
-  # Full build of ffmpeg
   buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.libvmaf ];
   configureFlags = (builtins.filter (f: f != "--disable-libvmaf") (old.configureFlags or [ ])) ++ [
     "--enable-libvmaf"
