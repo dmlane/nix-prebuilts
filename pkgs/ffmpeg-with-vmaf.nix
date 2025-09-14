dd1366fa9a81fdd34f04cdcb1d74fb9312cf796f
David Lane <git2018@dmlane.net>
2025-09-14 17:30:47 +0100

    Pins last 5 versions in cachix

--- pkgs/ffmpeg-with-vmaf.nix
+++ pkgs/ffmpeg-with-vmaf.nix
@@ -1,5 +1,6 @@
 { pkgs }:
 pkgs.ffmpeg-full.overrideAttrs (old: {
+  # ffmpeg with vmaf
   buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.libvmaf ];
   configureFlags = (builtins.filter (f: f != "--disable-libvmaf") (old.configureFlags or [ ])) ++ [
     "--enable-libvmaf"
